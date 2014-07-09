d505737eb442cddc803ecec3d9159392db18b64f
Yeshen <shangsunset@gmail.com>
2014-07-09 18:03:15 -0400

    moved to another repo. changed to carousel

---
+++ static/js/main.js
@@ -0,0 +1,44 @@
+/*====================================
+=            ON DOM READY            =
+====================================*/
+$(function() {
+    $('.toggle-nav').click(function() {
+        // Calling a function in case you want to expand upon this.
+        toggleNav();
+
+    });
+});
+
+$(document).keyup(function(e) {
+    if (e.keyCode == 27) {
+        if ($('#site-wrapper').hasClass('show-nav')) {
+            // Assuming you used the function I made from the demo
+            toggleNav();
+        }
+    } 
+});
+
+
+
+$('.carousel').carousel();
+
+var scene = document.getElementById('scene');
+var parallax = new Parallax(scene);
+
+
+
+/*========================================
+=            CUSTOM FUNCTIONS            =
+========================================*/
+function toggleNav() {
+    if ($('#site-wrapper').hasClass('show-nav')) {
+        // Do things on Nav Close
+        $('#site-wrapper').removeClass('show-nav');
+    } else {
+        // Do things on Nav Open
+        $('#site-wrapper').addClass('show-nav');
+    }
+	$('.menu-icon').toggleClass('fa-angle-double-left, fa-angle-double-right');
+
+}
+
