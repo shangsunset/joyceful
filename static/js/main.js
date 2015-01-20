0b3e545cfd791d7363cb20d041da3cdd5c9615ed
Yeshen Shang <shangsunset@gmail.com>
2015-01-19 20:57:44 -0500

    grid item animation

--- static/js/main.js
+++ static/js/main.js
@@ -127,34 +127,6 @@ $(function(){
 });
 
 
-function toggleNav() {
-    if ($('#site-wrapper').hasClass('show-nav')) {
-        // Do things on Nav Close
-        $('#site-wrapper').removeClass('show-nav');
-    } else {
-        // Do things on Nav Open
-        $('#site-wrapper').addClass('show-nav');
-    }
-	$('.menu-icon').toggleClass('fa-angle-double-left, fa-angle-double-right');
-
-}
 
 
-
-
-//postions of album cover text
-function centerElement(target) {
-
-    var element = $(target);
-    var elementHeight = element.height();
-    var windowHeight = $(window).height();
-    var elementPosTop = windowHeight/2-elementHeight/2;
-
-    var elementWidth = element.width();
-    var windowWidth = $(window).width();
-    var elementPosLeft = windowWidth/2-elementWidth/2;
-
-    // element.css({left: elementPosLeft, top: elementPosTop})
-    element.css({left: elementPosLeft});
-}
 
