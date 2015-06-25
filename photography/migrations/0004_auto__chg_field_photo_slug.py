9b6e815fa2d6b962edc5ca5e572698382f4ba2d1
shangsunset <shangsunset@gmail.com>
2015-06-24 21:21:02 -0400

    added migration files

---
+++ photography/migrations/0004_auto__chg_field_photo_slug.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Photo.slug'
+        db.alter_column(u'photography_photo', 'slug', self.gf('django.db.models.fields.SlugField')(max_length=50, null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Photo.slug'
+        db.alter_column(u'photography_photo', 'slug', self.gf('django.db.models.fields.SlugField')(default=datetime.datetime(2015, 6, 25, 0, 0), max_length=50))
+
+    models = {
+        u'photography.album': {
+            'Meta': {'ordering': "['-created']", 'object_name': 'Album'},
+            'album_cover': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'created': ('django.db.models.fields.DateField', [], {}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '400', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '120', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '40'})
+        },
+        u'photography.photo': {
+            'Meta': {'ordering': "['-created']", 'object_name': 'Photo'},
+            'album': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'photos'", 'to': u"orm['photography.Album']"}),
+            'caption': ('django.db.models.fields.CharField', [], {'max_length': '400', 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'null': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '120', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['photography']
