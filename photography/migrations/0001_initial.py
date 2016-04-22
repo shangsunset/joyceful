2f52fde53b2e35382095d4e10608701c1cea8be3
Yeshen Shang <shangsunset@gmail.com>
2016-04-22 13:56:16 -0400

    profile

--- photography/migrations/0001_initial.py
+++ photography/migrations/0001_initial.py
@@ -11,8 +11,8 @@ class Migration(SchemaMigration):
         # Adding model 'Album'
         db.create_table(u'photography_album', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=120, blank=True)),
-            ('description', self.gf('django.db.models.fields.CharField')(max_length=400, null=True, blank=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=120)),
+            ('description', self.gf('django.db.models.fields.TextField')(max_length=400, null=True, blank=True)),
             ('album_cover', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
             ('created', self.gf('django.db.models.fields.DateField')()),
             ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=40)),
@@ -23,11 +23,11 @@ class Migration(SchemaMigration):
         db.create_table(u'photography_photo', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('album', self.gf('django.db.models.fields.related.ForeignKey')(related_name='photos', to=orm['photography.Album'])),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=120, blank=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=120)),
             ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
             ('caption', self.gf('django.db.models.fields.CharField')(max_length=400, null=True, blank=True)),
             ('created', self.gf('django.db.models.fields.DateField')()),
-            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=40)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50)),
         ))
         db.send_create_signal(u'photography', ['Photo'])
 
@@ -45,9 +45,9 @@ class Migration(SchemaMigration):
             'Meta': {'ordering': "['-created']", 'object_name': 'Album'},
             'album_cover': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
             'created': ('django.db.models.fields.DateField', [], {}),
-            'description': ('django.db.models.fields.CharField', [], {'max_length': '400', 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '400', 'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '120', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '120'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '40'})
         },
         u'photography.photo': {
@@ -57,8 +57,8 @@ class Migration(SchemaMigration):
             'created': ('django.db.models.fields.DateField', [], {}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '40'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '120', 'blank': 'True'})
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '120'})
         }
     }
 
